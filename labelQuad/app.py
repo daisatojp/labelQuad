6a7f73cbcfcd7e71fc0f94b9b915f4a83587d471
Dai Sato <sato-da@sanritz.co.jp>
2025-03-05 19:20:15 +0900

    [maintenance] write down LabelFile class

--- labelQuad/app.py
+++ labelQuad/app.py
@@ -1,13 +1,16 @@
+import base64
 import contextlib
 import copy
 from functools import partial
 import html
+import io
 import json
 import math
 import os
 import os.path as osp
 import re
 from typing import Optional
+import PIL.Image
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -20,8 +23,6 @@ import labelme
 from labelme import __appname__
 from labelme import utils
 from labelme.config import get_config
-from labelme.label_file import LabelFile
-from labelme.label_file import LabelFileError
 from labelme.widgets import BrightnessContrastDialog
 from labelme.widgets import LabelDialog
 from labelme.widgets import LabelListWidget
@@ -32,8 +33,12 @@ from labelme.widgets import ZoomWidget
 import skimage.measure
 
 
+PIL.Image.MAX_IMAGE_PIXELS = None
+
+
 __appname__ = 'labelQuad'
 __version__ = '1.0.0'
+
 
 LABEL_COLORMAP = imgviz.label_colormap()
 ZOOM_MODE_FIT_WINDOW = 0
@@ -831,7 +836,7 @@ class Canvas(QWidget):
                     if self.createMode == "polygon":
                         self.current.addPoint(self.line[1])
                         self.line[0] = self.current[-1]
-                        if self.current.isClosed():
+                        if len(self.current.points) == 4:
                             self.finalise()
                     elif self.createMode in ["rectangle", "circle", "line"]:
                         assert len(self.current.points) == 1
@@ -1436,6 +1441,170 @@ class Canvas(QWidget):
         self.pixmap = None
         self.shapesBackups = []
         self.update()
+
+
+class LabelFileError(Exception):
+    pass
+
+
+class LabelFile(object):
+    suffix = ".json"
+
+    def __init__(self, filename=None):
+        self.shapes = []
+        self.imagePath = None
+        self.imageData = None
+        if filename is not None:
+            self.load(filename)
+        self.filename = filename
+
+    @staticmethod
+    def load_image_file(filename):
+        try:
+            image_pil = PIL.Image.open(filename)
+        except IOError:
+            logger.error("Failed opening image file: {}".format(filename))
+            return
+
+        # apply orientation to image according to exif
+        image_pil = utils.apply_exif_orientation(image_pil)
+
+        with io.BytesIO() as f:
+            ext = osp.splitext(filename)[1].lower()
+            if ext in [".jpg", ".jpeg"]:
+                format = "JPEG"
+            else:
+                format = "PNG"
+            image_pil.save(f, format=format)
+            f.seek(0)
+            return f.read()
+
+    def load(self, filename):
+        keys = [
+            "version",
+            "imageData",
+            "imagePath",
+            "shapes",  # polygonal annotations
+            "flags",  # image level flags
+            "imageHeight",
+            "imageWidth",
+        ]
+        shape_keys = [
+            "label",
+            "points",
+            "group_id",
+            "shape_type",
+            "flags",
+            "description",
+            "mask",
+        ]
+        try:
+            with open(filename, "r") as f:
+                data = json.load(f)
+
+            if data["imageData"] is not None:
+                imageData = base64.b64decode(data["imageData"])
+            else:
+                # relative path from label file to relative path from cwd
+                imagePath = osp.join(osp.dirname(filename), data["imagePath"])
+                imageData = self.load_image_file(imagePath)
+            flags = data.get("flags") or {}
+            imagePath = data["imagePath"]
+            self._check_image_height_and_width(
+                base64.b64encode(imageData).decode("utf-8"),
+                data.get("imageHeight"),
+                data.get("imageWidth"),
+            )
+            shapes = [
+                dict(
+                    label=s["label"],
+                    points=s["points"],
+                    shape_type=s.get("shape_type", "polygon"),
+                    flags=s.get("flags", {}),
+                    description=s.get("description"),
+                    group_id=s.get("group_id"),
+                    mask=utils.img_b64_to_arr(s["mask"]).astype(bool)
+                    if s.get("mask")
+                    else None,
+                    other_data={k: v for k, v in s.items() if k not in shape_keys},
+                )
+                for s in data["shapes"]
+            ]
+        except Exception as e:
+            raise LabelFileError(e)
+
+        otherData = {}
+        for key, value in data.items():
+            if key not in keys:
+                otherData[key] = value
+
+        # Only replace data after everything is loaded.
+        self.flags = flags
+        self.shapes = shapes
+        self.imagePath = imagePath
+        self.imageData = imageData
+        self.filename = filename
+        self.otherData = otherData
+
+    @staticmethod
+    def _check_image_height_and_width(imageData, imageHeight, imageWidth):
+        img_arr = utils.img_b64_to_arr(imageData)
+        if imageHeight is not None and img_arr.shape[0] != imageHeight:
+            logger.error(
+                "imageHeight does not match with imageData or imagePath, "
+                "so getting imageHeight from actual image."
+            )
+            imageHeight = img_arr.shape[0]
+        if imageWidth is not None and img_arr.shape[1] != imageWidth:
+            logger.error(
+                "imageWidth does not match with imageData or imagePath, "
+                "so getting imageWidth from actual image."
+            )
+            imageWidth = img_arr.shape[1]
+        return imageHeight, imageWidth
+
+    def save(
+        self,
+        filename,
+        shapes,
+        imagePath,
+        imageHeight,
+        imageWidth,
+        imageData=None,
+        otherData=None,
+        flags=None,
+    ):
+        if imageData is not None:
+            imageData = base64.b64encode(imageData).decode("utf-8")
+            imageHeight, imageWidth = self._check_image_height_and_width(
+                imageData, imageHeight, imageWidth
+            )
+        if otherData is None:
+            otherData = {}
+        if flags is None:
+            flags = {}
+        data = dict(
+            version=__version__,
+            flags=flags,
+            shapes=shapes,
+            imagePath=imagePath,
+            imageData=imageData,
+            imageHeight=imageHeight,
+            imageWidth=imageWidth,
+        )
+        for key, value in otherData.items():
+            assert key not in data
+            data[key] = value
+        try:
+            with open(filename, "w") as f:
+                json.dump(data, f, ensure_ascii=False, indent=2)
+            self.filename = filename
+        except Exception as e:
+            raise LabelFileError(e)
+
+    @staticmethod
+    def is_label_file(filename):
+        return osp.splitext(filename)[1].lower() == LabelFile.suffix
 
 
 class MainWindow(QMainWindow):
