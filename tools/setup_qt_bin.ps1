b8a17db96d357fd47d83ca782b2115186297a8da
Dai Sato <sato-da@sanritz.co.jp>
2025-03-06 15:34:59 +0900

    [enhancement] add labelQuad.spec to make executable

---
+++ tools/setup_qt_bin.ps1
@@ -0,0 +1,7 @@
+Copy-Item C:\Windows\System32\concrt140.dll .\.venv\Lib\site-packages\PyQt5\Qt5\bin\
+Copy-Item C:\Windows\System32\d3dcompiler_47.dll .\.venv\Lib\site-packages\PyQt5\Qt5\bin\
+Copy-Item C:\Windows\System32\msvcp140_1.dll .\.venv\Lib\site-packages\PyQt5\Qt5\bin\
+Copy-Item C:\Windows\System32\msvcp140_2.dll .\.venv\Lib\site-packages\PyQt5\Qt5\bin\
+Copy-Item C:\Windows\System32\msvcp140.dll .\.venv\Lib\site-packages\PyQt5\Qt5\bin\
+Copy-Item C:\Windows\System32\vcruntime140_1.dll .\.venv\Lib\site-packages\PyQt5\Qt5\bin\
+Copy-Item C:\Windows\System32\vcruntime140.dll .\.venv\Lib\site-packages\PyQt5\Qt5\bin\
