8dcaf2c726842752b8f7bd770a1ed2f520f6b52d
Dai Sato <sato-da@sanritz.co.jp>
2025-03-06 10:46:03 +0900

    [maintenance] write down ToolBar

--- labelQuad/app.py
+++ labelQuad/app.py
@@ -23,7 +23,6 @@ import numpy as np
 import yaml
 import labelme
 from labelme import utils
-from labelme.widgets import ToolBar
 from labelme.widgets import UniqueLabelQListWidget
 from labelme.widgets import ZoomWidget
 import skimage.measure
@@ -47,6 +46,30 @@ CURSOR_DRAW = Qt.CrossCursor
 CURSOR_MOVE = Qt.ClosedHandCursor
 CURSOR_GRAB = Qt.OpenHandCursor
 MOVE_SPEED = 5.0
+
+
+class ToolBar(QToolBar):
+    def __init__(self, title):
+        super(ToolBar, self).__init__(title)
+        layout = self.layout()
+        m = (0, 0, 0, 0)
+        layout.setSpacing(0)
+        layout.setContentsMargins(*m)
+        self.setContentsMargins(*m)
+        self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
+
+    def addAction(self, action):
+        if isinstance(action, QWidgetAction):
+            return super(ToolBar, self).addAction(action)
+        btn = QToolButton()
+        btn.setDefaultAction(action)
+        btn.setToolButtonStyle(self.toolButtonStyle())
+        self.addWidget(btn)
+
+        # center align
+        for i in range(self.layout().count()):
+            if isinstance(self.layout().itemAt(i).widget(), QToolButton):
+                self.layout().itemAt(i).setAlignment(Qt.AlignCenter)
 
 
 class Shape(object):
